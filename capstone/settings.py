4381618e2496c2690abc870df933d6e951df40da
John Wyse <john.w.wyse@gmail.com>
2025-07-27 17:11:29 -0500

    base64 update

--- capstone/settings.py
+++ capstone/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# File upload info
+# File upload info (still needed for lease documents and legacy images)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
